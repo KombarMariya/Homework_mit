a38cadf2b7e99b2f01c4002cdfaf5bcdce881498
KombarMariya <kombar.mariya@yandex.ru>
2023-04-25 16:44:57 +0400

    Добавление возможности управлять ракеткой

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -30,8 +30,24 @@ class Paddle:
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
-    def draw(self):
-        pass
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+    def draw(self): # перемещение ракетки
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
+    # смена направления
+    def turn_left(self, evt):
+        self.x = -2
+    def turn_right(self, evt):
+        self.x = 2
+
 
 tk = Tk()
 tk.title("Игра") # заголовок игрового окна
