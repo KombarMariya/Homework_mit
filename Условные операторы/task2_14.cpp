8df19f12f49f28ed2a1f9ff34012b8a39b6539be
KombarMariya <kombar.mariya@yandex.ru>
2023-05-08 11:28:21 +0400

    Использование условного оператора

---
+++ Условные операторы/task2_14.cpp
@@ -0,0 +1,63 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	cout << "1. Круг\n2. Квадрат\n3. Треугольник\n4. Прямоугольник\n";  // меню
+	int a; 
+	cout << "Введите номер фигуры ";
+	cin >> a;
+	if (a == 1) { // круг
+		float r;
+		cout << "Введите радиус круга ";
+		cin >> r;
+		float S = M_PI * (r*r);
+		float P = 2 * M_PI*r;
+		cout << "Площадь круга " << S << endl;
+		cout << "Периметр круга " << P;
+	}
+	else if (a == 2) { // квадрат
+		float b;
+		cout << "Введите сторону квадрата ";
+		cin >> b;
+		float S = b * b;
+		float P = 4 * b;
+		cout << "Площадь квадрата " << S << endl;
+		cout << "Периметр квадрата " << P;
+	}
+	else if (a == 3) { //треугольник
+		float x, y, z;
+		cout << "Введите первую сторону треугольника ";
+		cin >> x;
+		cout << "Введите вторую сторону треугольника ";
+		cin >> y;
+		cout << "Введите третью сторону треугольника ";
+		cin >> z;
+		if (x + y <= z || x + z <= y || z + y <= x) {  // проверка на существование треугольника
+			cout << "Не существует треугольника ";
+		}
+		else {
+			float P = x + z + y;
+			float Pl = P / 2;
+			float S = sqrt(Pl*(Pl - x)*(Pl - y)*(Pl - z));
+
+			cout << "Площадь треугольника " << S << endl;
+			cout << "Периметр треугольника " << P;
+		}
+	}
+	else if (a == 4) { // прямоугольник
+		float d, h;
+		cout << "Введите длину прямоугольника ";
+		cin >> d;
+		cout << "Введите ширину прямоугольника ";
+		cin >> h;
+		float S = d * h;
+		float P = 2 * (d + h);
+		cout << "Площадь прямоугольника " << S << endl;
+		cout << "Периметр прямоугольника " << P;
+	}
+	system("pause");
+	return 0;
+
+}
---
+++ Условные операторы/task2_9.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	float X;
+	cout << "Введите число ";
+	cin >> X;
+	const float eps = 0.00001;
+	if (abs(X + 3) < eps || abs(X - 3) < eps) {
+		cout << "Деление на ноль ";
+	}
+	else if (abs(X + 1) < eps) {
+		cout << "Логарифм из неположительного числа ";
+	}
+	else if ((X < 3) || ((X > 3) && (X < 5))) {
+		cout << "Корень из отрицательного числа ";
+	}
+	else {
+		float F = sqrt((X - 5) / (X*X - 9)) + log(X*X + 2 * X + 1);
+		cout << F;
+	}
+	return 0;
+}
